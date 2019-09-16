74136eb182e190170358ff1d70159094bf4045eb
Justin Osterholt <shroder@gmail.com>
2019-09-16 07:46:01 -0700

    Fixing cat-induced typo

--- src/forms/custom-form-1/index.tsx
+++ src/forms/custom-form-1/index.tsx
@@ -16,7 +16,7 @@ const form = function () {
     };
 
     const onSubmitParentHandler = () => {
-        //const [formValues, setFormValues] = useState<IFormValues | undefined>({});
+        const [formValues, setFormValues] = useState<IFormValues | undefined>({});
         console.log(formValues);
     }
 
